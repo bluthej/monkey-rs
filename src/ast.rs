76d434ab184ff27df062896a50eb964f36d15a4f
bluthej <joffrey.bluthe@e.email>
2023-06-15 21:13:13 +0200

    Add basic AST and parser (only let statements)

---
+++ src/ast.rs
@@ -0,0 +1,16 @@
+pub enum Statement<'a> {
+    LetStatement {
+        identifier: Identifier<'a>,
+        value: Expression,
+    },
+}
+
+pub struct Identifier<'a> {
+    pub value: &'a str,
+}
+
+pub struct Expression;
+
+pub struct Program<'a> {
+    pub statements: Vec<Statement<'a>>,
+}
---
+++ src/parser.rs
@@ -0,0 +1,135 @@
+#![allow(unused)]
+
+use crate::{
+    ast::{Expression, Identifier, Program, Statement},
+    lexer::Lexer,
+    token::Token,
+};
+use std::{iter::Peekable, mem::discriminant};
+
+struct Parser<'a> {
+    l: Peekable<Lexer<'a>>,
+    errors: Vec<String>,
+}
+
+impl<'a> Parser<'a> {
+    fn new(l: Lexer<'a>) -> Self {
+        Self {
+            l: l.peekable(),
+            errors: vec![],
+        }
+    }
+
+    fn parse_program(&mut self) -> Program<'a> {
+        let mut statements = vec![];
+        while self.l.peek().is_some() {
+            if let Some(stmt) = self.parse_statement() {
+                statements.push(stmt);
+            }
+        }
+        Program { statements }
+    }
+
+    fn parse_statement(&mut self) -> Option<Statement<'a>> {
+        match self.l.next() {
+            Some(Token::LET) => self.parse_let_statement(),
+            _ => None,
+        }
+    }
+
+    fn parse_let_statement(&mut self) -> Option<Statement<'a>> {
+        if !self.expect_peek(&Token::IDENT("")) {
+            return None;
+        }
+        let Some(Token::IDENT(value)) = self.l.next() else {
+            unreachable!("Peeked first");
+        };
+        if !self.expect_peek(&Token::ASSIGN) {
+            return None;
+        }
+        self.l.find(|token| token == &Token::SEMICOLON);
+        let identifier = Identifier { value };
+        let value = Expression;
+        Some(Statement::LetStatement { identifier, value })
+    }
+
+    fn expect_peek(&mut self, expected: &Token) -> bool {
+        let actual_is_expected = match self.l.peek() {
+            Some(actual) => discriminant(actual) == discriminant(expected),
+            None => false,
+        };
+        if !actual_is_expected {
+            self.peek_error(expected);
+        }
+        actual_is_expected
+    }
+
+    fn peek_error(&mut self, expected: &Token) {
+        let msg = format!(
+            "expected next token to be {:?}, got {:?} instead",
+            Some(expected),
+            self.l.peek()
+        );
+        self.errors.push(msg);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::lexer::Lex;
+
+    use super::*;
+
+    #[test]
+    fn let_statement() {
+        let input = "
+let x = 5;
+let y = 10;
+let foobar = 838383;
+";
+
+        let mut p = Parser::new(input.tokens());
+        let program = p.parse_program();
+
+        check_parser_errors(&p);
+
+        assert_eq!(program.statements.len(), 3);
+
+        let expected_identifiers = ["x", "y", "foobar"];
+        for (statement, expected_identifier) in program.statements.iter().zip(expected_identifiers)
+        {
+            let Statement::LetStatement {
+                identifier: name, ..
+            } = statement;
+            println!("{}", name.value);
+            println!("{}", expected_identifier);
+            assert_eq!(name.value, expected_identifier);
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn wrong_let_statement() {
+        let input = "
+let x 5;
+let = 10;
+let 838383;
+";
+
+        let mut p = Parser::new(input.tokens());
+        let program = p.parse_program();
+
+        check_parser_errors(&p);
+    }
+
+    fn check_parser_errors(p: &Parser) {
+        if p.errors.is_empty() {
+            return;
+        }
+        println!("parser has {} errors", p.errors.len());
+        for error in &p.errors {
+            eprintln!("{}", error);
+        }
+        panic!()
+    }
+}
